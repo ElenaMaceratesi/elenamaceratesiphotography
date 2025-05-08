ae8604ff4803bccb17eec64d2df0f1930d958ec0
Federico Del Piano <delpiano.fred@gmail.com>
2025-05-08 15:29:05 +0200

    fix: follow me + footer

--- src/app/components/home/about-me/about-me.component.ts
+++ src/app/components/home/about-me/about-me.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
