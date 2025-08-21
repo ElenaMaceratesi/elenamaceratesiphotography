1aedb6dda108dcac3b6c36a3aa292ea3d7f2efcb
Elena <elenamaceratesiphotography@gmail.com>
2025-08-21 21:12:14 +0200

    Modifica risoluzione foto

--- src/app/helper/servizi/servizi-helper.service.ts
+++ src/app/helper/servizi/servizi-helper.service.ts
@@ -8,47 +8,47 @@ export class ServiziHelperService {
 
   private serviziFotografici: ServizioFotografico[] = [
     {
-      foto: '/assets/home/servizi/maternita2.jpg',
+      foto: '/assets/servizi/elenco/maternita.jpg',
       servizio: 'Maternità',
       descrizione: 'Tra le 28esima e la 34esima settimana di gravidanza'
     },
     {
-      foto: '/assets/home/servizi/newborn2.jpg',
+      foto: '/assets/servizi/elenco/newborn.jpg',
       servizio: 'Newborn',
       descrizione: 'Entro il primo mese di vita'
     },
     {
-      foto: '/assets/home/servizi/primimesi.jpg',
+      foto: '/assets/servizi/elenco/primimesi.jpg',
       servizio: 'Primi mesi',
       descrizione: 'Da 1 a 5 mesi',
       path: '/primi-mesi'
     },
     {
-      foto: '/assets/home/servizi/bebe.jpg',
+      foto: '/assets/servizi/elenco/bebe.jpg',
       servizio: 'Bebè',
       descrizione: 'Da 6 agli 11 mesi',
       path: '/bebe'
     },
     {
-      foto: '/assets/home/servizi/smashcake2.jpg',
+      foto: '/assets/servizi/elenco/smashcake.jpg',
       servizio: 'Smash cake',
       descrizione: 'Per festeggiare il primo compleanno'
     },
     {
-      foto: '/assets/home/servizi/famiglia.jpg',
+      foto: '/assets/servizi/elenco/famiglia.jpg',
       servizio: 'Famiglia',
       descrizione: 'Da 1 anno in poi'
     },
     {
-      foto: '/assets/home/servizi/esterna.jpg',
+      foto: '/assets/servizi/elenco/esterna.jpg',
       servizio: 'Servizi in esterna'
     },
     {
-      foto: '/assets/home/servizi/minisessioni.jpg',
+      foto: '/assets/servizi/elenco/minisessioni.jpg',
       servizio: 'Mini sessioni'
     },
     {
-      foto: '/assets/home/servizi/ritratto.jpg',
+      foto: '/assets/servizi/elenco/ritratto.jpg',
       servizio: 'Ritratto'
     }
   ];
